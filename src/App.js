e3d393ed8583fbbcfcf6a19a15ba7d40f815835f
willardnyamombe <nya19001@byui.edu>
2022-06-12 16:13:32 -0600

    added components folder inside src folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import GoalList from './components/GoalList'
 
 const App = () => {
-  return <div>
-    <h2>My Goals</h2>
-    <ul>
-      <li>I want to finish the course</li>
-      <li>Learn all the course main topics</li>
-      <li>Be the best so I can get an internship</li>
-    </ul>
-  </div>;
+  return (
+    <div className='course-goals'>
+      <h2>My Goals</h2>
+      <GoalList />
+    </div>
+    );
 };
 
 export default App;
---
+++ src/components/GoalList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './GoalList.css';
+
+const GoalList = () => {
+    return (
+        <ul className='goal-list'>
+        <li>I want to finish the course</li>
+        <li>Learn all the course main topics</li>
+        <li>Be the best so I can get an internship and course Q&amp;A</li>
+      </ul>
+    );
+}
+
+export default GoalList;
