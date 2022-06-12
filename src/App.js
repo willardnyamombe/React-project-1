20458299a74886379c60055fadec72ebf63941fc
willardnyamombe <nya19001@byui.edu>
2022-06-12 14:53:54 -0600

    My first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return <div>
+    <h2>My Goals</h2>
+    <ul>
+      <li>I want to finish the course</li>
+      <li>Learn all the course main topics</li>
+      <li>Be the best so I can get an internship</li>
+    </ul>
+  </div>;
+};
+
+export default App;
