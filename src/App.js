715d5ee42b0531cec806eeadb8d6138cc11f4d31
willardnyamombe <nya19001@byui.edu>
2022-06-12 17:17:22 -0600

    rendering list of data

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './App.css';
 import GoalList from './components/GoalList'
 
 const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'I want to finish the course'},
+    {id: 'cg2', text: 'Learn all the course main topics'},
+    {id: 'cg3', text: 'Be the best so I can get an internship and course Q&A'}
+  ]
+
   return (
     <div className='course-goals'>
       <h2>My Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals}/>
     </div>
     );
 };
--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import './GoalList.css';
 
-const GoalList = () => {
+const GoalList = props => {
     return (
-        <ul className='goal-list'>
-        <li>I want to finish the course</li>
-        <li>Learn all the course main topics</li>
-        <li>Be the best so I can get an internship and course Q&amp;A</li>
+        <ul className='goal-list'>{
+        props.goals.map(goal => {
+            return <li key={goal.id}>{goal.text}</li>;
+        })
+        }
       </ul>
     );
 }
